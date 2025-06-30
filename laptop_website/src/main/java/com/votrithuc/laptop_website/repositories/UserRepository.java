707f5217343e2896205f83100eb8183163aaa6b4
vo tri thuc <trithucvo12345@gmail.com>
2025-07-01 02:28:03 +0700

    feat(repository): add or update repository interfaces

---
+++ laptop_website/src/main/java/com/votrithuc/laptop_website/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.votrithuc.laptop_website.repositories;
+
+import com.votrithuc.laptop_website.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+    // Các phương thức tùy chỉnh có thể được thêm ở đây
+}
